829dc69db812932f71f1578d614d9645a2ee7bc7
jhw <hyewon3085@naver.com>
2024-07-22 14:53:55 +0900

    feat/login-auth #3

--- models/user.js
+++ models/user.js
@@ -1,22 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
   static init(sequelize) {
-    return super.init({
-      id_user: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      inputid: { type: DataTypes.STRING, allowNull: false, unique:true },
-      nickname: { type: DataTypes.STRING, allowNull: false, unique:true },
-      email: { type: DataTypes.STRING, allowNull: false, unique:true },
-      password: { type: DataTypes.STRING, allowNull: false },
-    }, {
-      sequelize,
-      tableName: 'user',
-      timestamps: false
-    });
+    return super.init(
+      {
+        id_user: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        username: { type: DataTypes.STRING, allowNull: false, unique: true },
+        nickname: { type: DataTypes.STRING, allowNull: false, unique: true },
+        email: { type: DataTypes.STRING, allowNull: false, unique: true },
+        password: { type: DataTypes.STRING, allowNull: false },
+        // provider: {
+        //   type: DataTypes.ENUM('local', 'kakao'),
+        //   allowNull: false,
+        //   defaultValue: 'local',
+        // },
+        // snsId: { type: DataTypes.STRING, allowNull: true },
+      },
+      {
+        sequelize,
+        tableName: 'user',
+        timestamps: false,
+      }
+    );
   }
   static async hashPassword(password) {
     return await bcrypt.hash(password, 10);
   }
 }
- 
+
 module.exports = User;
