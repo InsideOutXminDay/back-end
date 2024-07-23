fb83de66afbb26ccc05114cc2d0997f3b76a67be
Hyemin Oh <orionrora@naver.com>
2024-07-23 16:20:05 +0900

    feat/add contents model #12

---
+++ models/contents.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Contents extends Model {
+  static init(sequelize) {
+    return super.init({
+      content_id: { type: DataTypes.INTEGER, primaryKey: true},
+      maincontent_id:{type: DataTypes.INTEGER, allowNull: false},
+      subcontent_id: { type: DataTypes.INTEGER, allowNull: false },
+      content_name: { type: DataTypes.STRING, allowNull: false },
+      subcontent_name : { type: DataTypes.STRING, allowNull: false },
+      content_link : {type: DataTypes.STRING, allowNull: false},
+      content_image : {type: DataTypes.STRING, allowNull: false},
+      subcontent_image : {type: DataTypes.STRING, allowNull: false},
+
+    }, {
+      sequelize,
+      tableName: 'contents',
+      timestamps: false
+    });
+  }
+
+}
+
+module.exports = Contents;
+
+
